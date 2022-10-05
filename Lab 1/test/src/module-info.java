4f18fad42cbc79889d239ffbef6548fda08a7ca4
Stoia George <stoiageorge@gmail.com>
2022-10-05 13:27:53 +0300

    Exemple Lab 1

---
+++ Lab 1/test/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author George
+ *
+ */
+module test {
+}
---
+++ Lab 1/test/src/test/Parent.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Parent {
+	private float age;
+
+	public float getAge() {
+		return age;
+	}
+
+	public void setAge(float age) {
+		this.age = age;
+	}
+
+	public Parent(float age) {
+		this.age = age;
+	}
+	
+	public Parent() {
+		this.age = 45;
+	}
+}
